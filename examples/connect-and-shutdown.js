02c2d1d04f2337699e7347b6092d8cbe7a481e16
Manuel Mitasch <manuel@Manuels-MacBook-Pro.local>
2019-12-14 12:14:30 +0100

    Add shutdown example

---
+++ examples/connect-and-shutdown.js
@@ -0,0 +1,24 @@
+// This example scans for 3 seconds to find juggglows
+// When it finds a juggglow it connects to it
+// When the juggglow is connected it then changes the color every second between red and white
+
+process.env.DEBUG = 'service,service:*,prop,prop:connect'; // activate debug messages
+
+var jService = require('../index.js');
+var colorToggle = true;
+
+jService.scan({ seconds: 3 })
+.catch(function(error) {
+  console.log('ERROR: ' + error);
+});
+
+jService.on('found', function(peripheral) {
+  return jService.connect({ peripheral: peripheral });
+});
+
+jService.on('connected', function(prop) {
+    setTimeout(function() {
+      prop.shutdown();
+    }, 2000);
+});
+
